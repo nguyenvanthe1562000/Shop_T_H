98cfae03b2a65d10187bd56bd5fa16ac6d8ac822
nguyenvanthe1562000 <nguyenvanthe1562000@gmail.com>
2020-10-25 14:38:56 +0700

    .

---
+++ ShopT_H.Service/ProductCategoryService.cs
@@ -0,0 +1,71 @@
+﻿using Shop_T_H.Data.Infrastructure;
+using Shop_T_H.Data.Repositories;
+using Shop_T_H.Model.Models;
+using System.Collections.Generic;
+
+namespace Shop_T_H.Service
+{
+    public interface IProductCatergoryService
+    {
+        ProductCategory Add(ProductCategory  productCategory);
+
+        void Update(ProductCategory postCategory);
+
+        ProductCategory Delete(int id);
+
+        IEnumerable<ProductCategory> GetAll();
+
+        IEnumerable<ProductCategory> GetAllByParentId(int parentId);
+
+        ProductCategory GetById(int id);
+
+        void Save();
+    }
+
+    public class ProductCatergoryService : IProductCatergoryService
+    {
+        private IProductCategoryRepository _productCategoryRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public ProductCatergoryService(IProductCategoryRepository productCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._productCategoryRepository = productCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public ProductCategory Add(ProductCategory productCategory)
+        {
+            return _productCategoryRepository.Add(productCategory);
+        }
+
+        public ProductCategory Delete(int id)
+        {
+            return _productCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<ProductCategory> GetAll()
+        {
+            return _productCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<ProductCategory> GetAllByParentId(int parentId)
+        {
+            return _productCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentId);
+        }
+
+        public ProductCategory GetById(int id)
+        {
+            return _productCategoryRepository.GetSingleById(id);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(ProductCategory productCategory)
+        {
+            _productCategoryRepository.Update(productCategory);
+        }
+    }
+}
---
+++ ShopT_H.Web/Api/ProductCategoryController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Shop_T_H.Model.Models;
+using Shop_T_H.Service;
+using Shop_T_H.Web.Infrastructure.Core;
+using Shop_T_H.Web.Models;
+
+namespace Shop_T_H.Web.Api
+{
+    [RoutePrefix("api/productcategory")]
+    public class ProductCategoryController : ApiControllerBase
+    {
+        IProductCatergoryService _productCategoryService;
+        public ProductCategoryController(IErrorService errorService, IProductCatergoryService productCatergoryService) :base(errorService)
+        {
+            _productCategoryService = productCatergoryService;
+        }
+        [Route("getall")]
+        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productCategoryService.GetAll();
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                return response;
+            });
+        }
+    }
+}
